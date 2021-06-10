3bcc4ed6f6377179a44ef40524576586f23c9db5
Aluru Murali Krishna <alurumuralikrishna9@gmail.com>
2021-06-10 23:42:02 +0530

    Added ChakraUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,43 @@
-import { AuthProvider } from '../lib/auth';
-import '../styles/globals.css';
+import { CSSReset } from '@chakra-ui/css-reset';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { AuthProvider } from '@/lib/auth';
+import '@/styles/globals.css';
+import theme from '@/styles/theme';
+import { Global, css } from '@emotion/react';
+// import { Global } from '@emotion/react';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <>
+          <GlobalStyle />
+        </>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,30 @@
+import { Code, Button, Text, Heading } from '@chakra-ui/react';
+
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+import { useAuth } from '@/lib/auth';
 
 export default function Home() {
   const auth = useAuth();
   console.log(auth);
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Fast Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback</h1>
+      <main>
+        <Heading>Fast Feedback</Heading>
 
-        <div>
-          {!auth.user && (
-            <button onClick={(e) => auth.signinWithGithub()}>Sign In</button>
-          )}
-        </div>
+        <Text>
+          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
+        </Text>
 
-        <div>{auth?.user?.email}</div>
-
-        <div>
-          {auth?.user && (
-            <button onClick={(e) => auth.signout()}>SignOut</button>
-          )}
-        </div>
+        {auth.user ? (
+          <Button onClick={(e) => auth.signout()}>SignOut</Button>
+        ) : (
+          <Button onClick={(e) => auth.signinWithGithub()}>Sign In</Button>
+        )}
       </main>
     </div>
   );
